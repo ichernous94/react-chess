7f42a347128ce81f123c3132b3af50b6a56b16a0
Igor Chernous <ichernous94@gmail.com>
2022-10-19 01:50:13 +0300

    feat: add constructor for all chess figures

--- src/models/figures/Bishop.ts
+++ src/models/figures/Bishop.ts
@@ -1,3 +1,13 @@
-import { Figure } from './Figure';
+import { Cell } from '../Cell';
+import { Colors } from '../Colors';
+import { Figure, FigureNames } from './Figure';
+import blackLogo from '../../assets/black-bishop.png';
+import whiteLogo from '../../assets/white-bishop.png';
 
-export class Bishop extends Figure {}
+export class Bishop extends Figure {
+  constructor(color: Colors, cell: Cell) {
+    super(color, cell); // extends Figure
+    this.logo = color === Colors.BLACK ? blackLogo : whiteLogo;
+    this.name = FigureNames.BISHOP;
+  }
+}
--- src/models/figures/King.ts
+++ src/models/figures/King.ts
@@ -1,3 +1,13 @@
-import { Figure } from './Figure';
+import { Cell } from '../Cell';
+import { Colors } from '../Colors';
+import { Figure, FigureNames } from './Figure';
+import blackLogo from '../../assets/black-king.png';
+import whiteLogo from '../../assets/white-king.png';
 
-export class Bishop extends Figure {}
+export class King extends Figure {
+  constructor(color: Colors, cell: Cell) {
+    super(color, cell); // extends Figure
+    this.logo = color === Colors.BLACK ? blackLogo : whiteLogo;
+    this.name = FigureNames.KING;
+  }
+}
